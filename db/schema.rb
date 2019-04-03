1791a66bba2206f259172bc0cff29ab7c0d5b04e
Gingertonic <bethmschofield@gmail.com>
2019-04-03 14:06:46 +0100

    database update

--- db/schema.rb
+++ db/schema.rb
@@ -13,21 +13,27 @@
 ActiveRecord::Schema.define(version: 20180405145920) do
 
   create_table "artists", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "genres", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "song_genres", force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "songs", force: :cascade do |t|
+    t.string "title"
+    t.integer "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ep = Artist.create(name: 'Elvis')
+gt = Artist.create(name: 'Gingertonic')
+ven = Artist.create(name: 'Venetian Snares')
+ad = Artist.create(name: 'Adele')
+
+haj = ven.songs.create(title: 'Hajnal')
+hello = ad.songs.create(title: 'Hello')
+jc = gt.songs.create(title: 'James and Celia')
+jr = ep.songs.create(title: 'Jailhouse Rock')
+blue = ep.songs.create(title: 'Blue Suede Shoes')
+
+elec = Genre.create(name: 'electronic')
+rock = Genre.create(name: 'rock')
+folk = Genre.create(name: 'folk')
+pop = Genre.create(name: 'pop')
+blues = Genre.create(name: 'blues')
+
+haj.genres << elec
+hello.genres << pop
+jc.genres << pop
+jc.genres << folk
+jr.genres << rock
+jr.genres << pop
+blue.genres << blues
+blue.genres << rock
